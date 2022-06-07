6476769f6b7a1fc179c554b20f580834ac3bd1a5
Sema Adıbelli <semanur.adibelli@gmail.com>
2022-06-07 21:04:03 +0300

    Identity Ok.

---
+++ TechnicalService.Data/Identity/ApplicationRole.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selfy.Data.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public ApplicationRole()
+        {
+        }
+
+        public ApplicationRole(string roleName) : base(roleName)
+        {       
+        }
+
+
+    }
+}
