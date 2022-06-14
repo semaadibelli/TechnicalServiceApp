d0cddb154baf1c338161b8531d120ec58509a421
Sema Adıbelli <semanur.adibelli@gmail.com>
2022-06-14 17:59:16 +0300

    Controller Edit.

--- TechnicalService.Web/Controllers/HomeController.cs
+++ TechnicalService.Web/Controllers/HomeController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalService.Core.Identity;
+using TechnicalService.Data.Identity;
 
 namespace TechnicalService.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+
     }
 }
